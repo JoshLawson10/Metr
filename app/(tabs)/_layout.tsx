5ffe6994b2de5b3bade6d6ae8e051ece08804ed2
Josh Lawson <joshlawson100@outlook.com>
2026-04-30 22:45:56 +1000

    fix(routing): rename home to index

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function TabLayout() {
   return (
     <NativeTabs>
-      <NativeTabs.Trigger name="home">
+      <NativeTabs.Trigger name="index">
         <Label>Home</Label>
         <Icon sf="house.fill" drawable="custom_android_drawable" />
       </NativeTabs.Trigger>
