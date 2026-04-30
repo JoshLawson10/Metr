d2c1610c5c2129bbc48f7dcb09446f0ea2b446a0
Josh Lawson <joshlawson100@outlook.com>
2026-04-30 21:31:26 +1000

    feat(screens): set up placeholder screens

---
+++ app/(tabs)/setlist.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function SetlistScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Setlist Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+  },
+});
