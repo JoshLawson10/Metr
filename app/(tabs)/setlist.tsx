10cc316c0cc9e9038bd0639327637c1b51280ee7
Josh Lawson <joshlawson100@outlook.com>
2026-05-11 10:54:09 +1000

    feat(setlist): initial setlist screen creation

--- app/(tabs)/setlist.tsx
+++ app/(tabs)/setlist.tsx
@@ -1,20 +1,46 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, useWindowDimensions } from "react-native";
+import { ThemedView } from "@/components/ui/themed-view";
+import { ThemedText } from "@/components/ui/themed-text";
+import { GlassButton } from "@/components/ui/glass-button";
+import { BackgroundGlow } from "@/components/ui/background-glow";
+import { Plus } from "lucide-react-native";
 
 export default function SetlistScreen() {
+  const { width, height } = useWindowDimensions();
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Setlist Screen</Text>
-    </View>
+    <ThemedView style={styles.container}>
+      <BackgroundGlow
+        width={width}
+        height={height}
+        pos={{ x: width / 2, y: height / 2 }}
+        color="#4F46E5"
+      />
+      <View style={styles.header}>
+        <ThemedText size="h1">Setlist</ThemedText>
+        <GlassButton
+          onPress={() => {
+            console.log("Setlist button pressed");
+          }}
+        >
+          <Plus size={20} strokeWidth={3} color="#fff" />
+        </GlassButton>
+      </View>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
-  text: {
-    fontSize: 24,
+
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
   },
 });
